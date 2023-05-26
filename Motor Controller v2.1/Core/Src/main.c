314a811cd8882a50fb259ace8e2bdfe95a555ae8
JeroenvanDijk23 <crashoverride@planet.nl>
2023-05-26 22:54:08 +0200

    Update main.c

--- Motor Controller v2.1/Core/Src/main.c
+++ Motor Controller v2.1/Core/Src/main.c
@@ -2,16 +2,13 @@
 /**
   ******************************************************************************
   * @file           : main.c
-  * @brief          : Main program body
+  * @brief          : Bipolar Impulse COntroller
   ******************************************************************************
   * @attention
   *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
+  * AcidByte 2023
+	*
+	*	Impulse controller of the new generation
   *
   ******************************************************************************
   */
